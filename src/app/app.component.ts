246088d37056c41ddc4550812d1dc8df27eff4cf
YanniLI <840871885@qq.com>
2017-08-01 17:35:37 +0800

    开始敲一遍英雄教程

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,18 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'mr.nice' },
+  { id: 22, name: 'mr.nice2' },
+  { id: 33, name: 'mr.nice3' },
+  { id: 44, name: 'mr.nice4' },
+  { id: 55, name: 'mr.nice5' },
+  { id: 66, name: 'mr.nice6' },
+  { id: 77, name: 'mr.nice7' },
+  { id: 88, name: 'mr.nice8' },
+  { id: 99, name: 'mr.nice9' },
+  { id: 10, name: 'mr.nice10' }
+]
 
 @Component({
   selector: 'app-root',
@@ -6,5 +20,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'tour of heroes';
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
