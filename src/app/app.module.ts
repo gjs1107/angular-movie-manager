798df8b5dbffe29e20db823336e0afc63c4aef88
YanniLI <840871885@qq.com>
2017-08-03 17:21:49 +0800

    添加ngx-boostrap 和 boostrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
-import { SideBarComponent } from './common/sidebar/sidebar.component';
+import { SideBarComponent } from './pages/common/sidebar/sidebar.component';
 import { MovieAddrComponent } from './pages/movie/movie-add.component';
 import { GameAddrComponent } from './pages/game/game-add.component';
 import { ShowAddrComponent } from './pages/show/show-add.component';
@@ -21,6 +22,7 @@ import { AppRoutingModule }     from './router/app-routing.module';
     ShowAddrComponent
   ],
   imports: [
+    AlertModule.forRoot(),
     BrowserModule,
     FormsModule,
     AppRoutingModule
